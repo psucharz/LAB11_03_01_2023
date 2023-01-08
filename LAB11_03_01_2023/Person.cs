4aad4741cb5fe7ddcc6ed2095b24dbd7077d4313
psucharz <patryk.such25@gmail.com>
2023-01-08 17:30:36 +0100

    Create Person and Triple classes inheriting from Icomparable

---
+++ LAB11_03_01_2023/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB11_03_01_2023
+{
+    public class Person : IComparable<Person>
+    {
+        private DateTime _birthday;
+        private string _surname;
+
+        public Person(string name, string surname, DateTime birthday)
+        {
+            Name = name;
+            Surname = surname;
+            Birthday = birthday;
+        }
+
+        public String Name
+        { get; set; }
+
+        public String Surname
+        {
+            get
+            { return _surname; }
+            set
+            {
+                if (value != String.Empty) _surname = value;
+                else throw new ArgumentException($"{this.GetType()}, Surname cannot be empty");
+            }
+        }
+
+        public DateTime Birthday
+        {
+            get
+            { return _birthday; }
+            set
+            {
+                if (value <= DateTime.Today) _birthday = value;
+                else throw new ArgumentOutOfRangeException($"{this.GetType()}, Date out of valid birthday range");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname} {Birthday}";
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            int surameComparison = Surname.CompareTo(other.Surname);
+            if (surameComparison != 0)
+            {
+                return surameComparison;
+            }
+            int nameComparison = Name.CompareTo(other.Name);
+            if (nameComparison != 0)
+            {
+                return nameComparison;
+            }
+            return Birthday.CompareTo(other.Birthday);
+        }
+    }
+}
---
+++ LAB11_03_01_2023/Triple.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB11_03_01_2023
+{
+    public class Triple<T> where T : IComparable<T>
+    {
+        public Triple(T a, T b, T c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public T A
+        { get; set; }
+        public T B
+        { get; set; }
+        public T C
+        { get; set; }
+
+        public override string ToString()
+        {
+            return $"({A} , {B} , {C})";
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return A;
+                else if (index == 1)
+                    return B;
+                else if (index == 2)
+                    return C;
+                else
+                    throw new IndexOutOfRangeException($"No element in {this.GetType()} with given index");
+            }
+            set
+            {
+                if (index == 0)
+                    A = value;
+                else if (index == 1)
+                    B = value;
+                else if (index == 2)
+                    C = value;
+                else
+                    throw new IndexOutOfRangeException($"No element in {this.GetType()} with given index");
+            }
+        }
+
+        public void Sort()
+        {
+            if (A.CompareTo(B) > 0)
+                (B, A) = (A, B);
+            if (B.CompareTo(C) > 0)
+                (C, B) = (B, C);
+            if (A.CompareTo(C) > 0)
+                (C, A) = (A, C);
+        }
+    }
+}
